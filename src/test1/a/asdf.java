399ad93322623b15bd02eafa0d4a88fc13304464
Michiel@unhosted <michiel@unhosted.org>
2011-06-08 21:46:44 +0200

    display davUrl on the screen

--- src/test1/a/asdf.java
+++ src/test1/a/asdf.java
@@ -1,63 +1,41 @@
 package test1.a;
 
-import java.io.IOException;
 import java.net.URL;
-
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.InputSource;
 
- 
-
-
-
-
-
-
 public class asdf extends Activity {
-	private String getHostMeta(String domain) {
-		DefaultHttpClient httpclient = new DefaultHttpClient();
-		HttpGet httppost = new HttpGet("http://"+domain+"/.well-known/host-meta");
-		String responseBody;
-		try {
-			ResponseHandler<String> responseHandler=new BasicResponseHandler();
-			responseBody = httpclient.execute(httppost, responseHandler);
-		} catch (ClientProtocolException e) {
-			responseBody = "CPE";
-		} catch (IOException e) {
-			responseBody = "IOE";
-		}
-		return responseBody;
-	}
-
 	private String userAddressToDavUrl(String userAddress) {
 		try {
-			String[] parts = userAddress.split("@");
-			if(parts.length != 2) {
+			String[] userAddressParts = userAddress.split("@");
+			if(userAddressParts.length != 2) {
 				return "invalid user address";
 			}
-			URL hostMeta = new URL("http://"+ parts[1] + "/.well-known/host-meta");
-			SAXParserFactory spf = SAXParserFactory.newInstance();
-			SAXParser sp = spf.newSAXParser();
-			XMLReader xr = sp.getXMLReader();
+			URL hostMeta = new URL("http://"+ userAddressParts[1] + "/.well-known/host-meta");
+			XMLReader xr = SAXParserFactory.newInstance().newSAXParser().getXMLReader();
 			HostMetaParser myHostMetaParser = new HostMetaParser();
 			xr.setContentHandler(myHostMetaParser);
 			xr.parse(new InputSource(hostMeta.openStream()));
 			String template = myHostMetaParser.getLrddTemplate();
-			return template;
+			String[] templateParts = template.split("\\{uri\\}");
+			URL lrdd;
+			if(templateParts.length == 1) {
+				lrdd = new URL(templateParts[0] + userAddress);
+			} else if(templateParts.length == 2) {
+				lrdd = new URL(templateParts[0] + userAddress + templateParts[1]);
+			} else {
+				return "failed to parse lrdd template!";
+			}
+			LrddParser myLrddParser = new LrddParser();
+			xr.setContentHandler(myLrddParser);
+			xr.parse(new InputSource(lrdd.openStream()));
+			String davUrl = myLrddParser.getDavUrl();
+			return davUrl;
 		} catch (Exception e) {
 			return "Error!";
 		}
@@ -68,7 +46,7 @@ public class asdf extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
     	TextView tv = new TextView(this);
-        tv.setText(this.userAddressToDavUrl("mich@federoni.org"));
+        tv.setText(this.userAddressToDavUrl("mich@myfavouritesandwich.org"));
         setContentView(tv);
 	}
 }
