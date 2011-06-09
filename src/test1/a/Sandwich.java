e60d939967f32d5bb089d19ff9bd59a5d6c98750
Michiel@unhosted <michiel@unhosted.org>
2011-06-09 10:25:20 +0200

    separated 'unhosted' from 'sandwich'

--- src/test1/a/Sandwich.java
+++ src/test1/a/Sandwich.java
@@ -1,5 +1,22 @@
 package test1.a;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Sandwich {
-	public String[] ingredients;
+	public String[] ingredients = {"", ""};
+
+	public void fromJson(String json) {
+		JSONObject sandwichObj;
+		try {
+			sandwichObj = new JSONObject(json);
+			JSONArray ingredientsArr = sandwichObj.getJSONArray("ingredients");
+			this.ingredients[0] = ingredientsArr.getString(0);
+			this.ingredients[1] = ingredientsArr.getString(1);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
 }
--- src/test1/a/asdf.java
+++ src/test1/a/asdf.java
@@ -24,82 +24,11 @@ import org.xml.sax.InputSource;
 import com.google.gson.Gson;
 
 public class asdf extends Activity {
-	private String userAddressToDavUrl(String userAddress, String userDomain) {
-		try {
-			URL hostMeta = new URL("http://"+ userDomain + "/.well-known/host-meta");
-			XMLReader xr = SAXParserFactory.newInstance().newSAXParser().getXMLReader();
-			HostMetaParser myHostMetaParser = new HostMetaParser();
-			xr.setContentHandler(myHostMetaParser);
-			xr.parse(new InputSource(hostMeta.openStream()));
-			String template = myHostMetaParser.getLrddTemplate();
-			String[] templateParts = template.split("\\{uri\\}");
-			URL lrdd;
-			if(templateParts.length == 1) {
-				lrdd = new URL(templateParts[0] + userAddress);
-			} else if(templateParts.length == 2) {
-				lrdd = new URL(templateParts[0] + userAddress + templateParts[1]);
-			} else {
-				return "failed to parse lrdd template!";
-			}
-			LrddParser myLrddParser = new LrddParser();
-			xr.setContentHandler(myLrddParser);
-			xr.parse(new InputSource(lrdd.openStream()));
-			String davUrl = myLrddParser.getDavUrl();
-			return davUrl;
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SAXException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return "Error!";
-	}
-	
-	//isn't there a cleaner way of doing this?
-	private static String convertStreamToString(InputStream is) {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-		StringBuilder sb = new StringBuilder();
-		String line = null;
-		try {
-			while ((line = reader.readLine()) != null) {
-				sb.append(line + "\n");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				is.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return sb.toString();
-	}
-	 
+	private Unhosted unhosted;
 	private String[] getSandwichIngredients(String userAddress) {
-		String[] userAddressParts = userAddress.split("@");
-		if(userAddressParts.length != 2) {
-			String[] error = { "invalid", "user address" };
-			return error;
-		}
-		String davUrl = this.userAddressToDavUrl(userAddress, userAddressParts[1]);
-		try {
-			URL favSandwich = new URL(davUrl + "/webdav/" 
-					+ userAddressParts[1] 
-					+ "/" + userAddressParts[0] 
-					+ "/myfavouritesandwich.org/favSandwich");
-			Sandwich sandwich = new Gson().fromJson(
-						convertStreamToString(favSandwich.openStream()), Sandwich.class);
-			return sandwich.ingredients;
-		} catch(MalformedURLException e) {
-		} catch(IOException e) {
-		}
-		String[] unknown = { "ingredient 1", "ingredient 2" };
-		return unknown;
+		Sandwich sandwich = new Sandwich();
+		sandwich.fromJson(this.unhosted.get(userAddress, "myfavouritesandwich.org", "favSandwich"));
+		return sandwich.ingredients;
 	}
 
 	/** Called when the activity is first created. */
@@ -108,6 +37,8 @@ public class asdf extends Activity {
 		super.onCreate(savedInstanceState);
 		TextView tv = new TextView(this);
 		String userAddress = "mich@myfavouritesandwich.org";
+		String userPass = "asdf";
+		this.unhosted = new Unhosted(userAddress, userPass);
 		String[] ingredients = this.getSandwichIngredients(userAddress);
 		tv.setText("Logged in as "+userAddress+" - your sandwich has "+ingredients[0]+" and "+ingredients[1]+" on it");
 		setContentView(tv);
